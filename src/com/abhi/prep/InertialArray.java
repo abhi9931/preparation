869c6414233d6558dd2f44e176c610349802ccef
abhi1 <abhi1@DESKTOP-62GPL7H>
2019-06-23 14:24:47 +0530

    Adding java code for inertial and madhav array

---
+++ src/com/abhi/prep/InertialArray.java
@@ -0,0 +1,53 @@
+package com.abhi.prep;
+import java.util.Arrays;
+
+public class InertialArray {
+
+	public static int isInertial(int[] ar) {
+
+		if (null != ar && 0 < ar.length) {
+			Arrays.sort(ar);
+
+		}
+
+		boolean lastIndexEven = false;
+		boolean isInertial = true;
+		int retVal=1;
+		int length = ar.length;
+		// After sorting in ASC order checking is last index is an even number
+		if (ar[length - 1] % 2 == 0) {
+			lastIndexEven = true;
+		}else {
+			retVal=0;
+		}
+		if (lastIndexEven) {
+			for (int i = 0; i < length - 1; i++) {
+				// Checking if the array contains at least one odd number
+				if (ar[i] % 2 != 0) {
+					int oddNumber = ar[i];
+					// Checking if all the odd numbers are greater than all the even number except
+					// the biggest even number
+					for (int j = 0; j < length - 1; j++) {
+						// Checking if all the even numbers except the maximum number (occuring any
+						// number of times) are not bigger than odd number
+						if (ar[j] % 2 == 0 && ar[j] != ar[length - 1]) {
+							if (oddNumber < ar[j]) {
+								isInertial=false;
+								retVal= 0;
+								break;
+							}
+						}
+					}
+
+				}
+			}
+		}
+		//System.out.println("Is inertial   :   " + isInertial);
+		return retVal;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/com/abhi/prep/main/DriverClass.java
@@ -0,0 +1,14 @@
+package com.abhi.prep.main;
+
+import com.abhi.prep.InertialArray;
+import com.abhi.prep.MadhavArray;
+
+public class DriverClass {
+	public static void main(String[] args) {
+		int[] input = {2, 1, 1, 4, -1, -1}  ;
+	System.out.println("Is Inertial Array\t"+InertialArray.isInertial(input));
+	System.out.println("\nIs Madhav Array\t"+MadhavArray.isMadhavArray(input));
+	Math.floor(200);
+	}
+
+}
