98dc23900c95256e66901fb006093085f7fc9ab3
abhi1 <abhi1@DESKTOP-62GPL7H>
2019-06-30 20:35:25 +0530

    Formatted

--- src/com/abhi/prep/PrimeNumber.java
+++ src/com/abhi/prep/PrimeNumber.java
@@ -1,32 +1,30 @@
 package com.abhi.prep;
 
 public class PrimeNumber {
-public static int primeCount(int start, int end) {
-	int count=0;
-	for(int num=start;num<=end;num++) {
-		if(checkPrimeNumber(num)==true) {
-			count++;
+	public static int primeCount(int start, int end) {
+		int count = 0;
+		for (int num = start; num <= end; num++) {
+			if (checkPrimeNumber(num) == true) {
+				count++;
+			}
 		}
-	}
-	
-	
-	
-	return count;
-}
 
-public static boolean checkPrimeNumber(int num) {
-	int maxDividend=num/2+1;
-	if(num<=1) {
-		return false;
+		return count;
 	}
-	
-	for(int i=2;i<maxDividend;i++) {
-		if(num%i==0) {
+
+	public static boolean checkPrimeNumber(int num) {
+		int maxDivider = num / 2 + 1;
+		if (num <= 1) {
 			return false;
 		}
+
+		for (int i = 2; i < maxDivider; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
 	}
-	
-	return true;
-}
 
 }
