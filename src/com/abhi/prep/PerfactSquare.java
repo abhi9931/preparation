10734ed3b8b2e7ed9d62959ee43f59af58e1a77e
abhi1 <abhi1@DESKTOP-62GPL7H>
2019-07-09 01:05:12 +0530

    Adding java classes to find perfact square and NUnique array

--- src/com/abhi/prep/PerfactSquare.java
+++ src/com/abhi/prep/PerfactSquare.java
@@ -2,4 +2,16 @@ package com.abhi.prep;
 
 public class PerfactSquare {
 
+	
+	public static int isSquare(int n) {
+		
+		
+		for(int i=1;i*i<=n;i++) {
+			if(i*i==n) {
+				return 1;
+			}
+		}
+		
+		return 0;
+	}
 }
